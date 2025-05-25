1913d7db6d9365d6f740cc440d1c1753f81a05d7
Artur Gasparyan <a.gasparyan@itfbgroup.ru>
2025-05-25 12:00:42 +0300

    feature commit

---
+++ src/main/java/com/example/guysproject/service/PartnerTypeService.java
@@ -0,0 +1,16 @@
+package com.example.guysproject.service;
+
+import com.example.guysproject.entity.PartnerType;
+import com.example.guysproject.repository.PartnerTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PartnerTypeService {
+    private final PartnerTypeRepository repository;
+
+    public PartnerType createPartner(String name){
+        return repository.save(PartnerType.builder().name(name).build());
+    }
+}
